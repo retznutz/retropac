46ef009100485cc2b023a6a4f192a0b9aafb2125
Matt Retzer <matt@northernground.com>
2026-01-27 08:26:37 -0600

    Fixed head

---
+++ web/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        <a href="https://github.com/retznutz/retropac" target="_blank" rel="noopener">
+            <img src="~/assets/logo.svg" alt="RetroPac" class="header-logo" />
+        </a>
+        <nav class="header-nav">
+            <NuxtLink to="/" class="nav-link" :class="{ active: route.path === '/' }">
+                <i class="bx bx-movie-play"></i> Animations
+            </NuxtLink>
+            <NuxtLink to="/config" class="nav-link" :class="{ active: route.path === '/config' }">
+                <i class="bx bx-cog"></i> Config
+            </NuxtLink>
+        </nav>
+        <div class="header-actions">
+            <slot />
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
